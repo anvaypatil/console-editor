4d5a9a66b3490031341ddc75fb9224d11456d52b
Anvay <anvay13@gmail.com>
2022-09-17 09:53:11 +0530

    editor highlighting working

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,25 +1,17 @@
-use std::io;
-use std::io::{stdout, Write};
+use crate::Position;
+use std::io::{self, stdout, Write};
 use termion::color;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
-use crate::editor::Position;
 
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
-
 pub struct Terminal {
     size: Size,
     _stdout: RawTerminal<std::io::Stdout>,
-}
-
-impl Terminal {
-    pub fn clear_current_line() {
-        print!("{}", termion::clear::CurrentLine);
-    }
 }
 
 impl Terminal {
@@ -33,24 +25,24 @@ impl Terminal {
             _stdout: stdout().into_raw_mode()?,
         })
     }
-
     pub fn size(&self) -> &Size {
         &self.size
     }
-
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
-    pub fn flush() -> Result<(), std::io::Error> {
-        io::stdout().flush()
-    }
+
+    #[allow(clippy::cast_possible_truncation)]
     pub fn cursor_position(position: &Position) {
-        let Position { x, y } = position;
-        let x = x.saturating_add(1);
-        let y = y.saturating_add(1);
+        let Position { mut x, mut y } = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
         let x = x as u16;
         let y = y as u16;
         print!("{}", termion::cursor::Goto(x, y));
+    }
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
     }
     pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
@@ -65,19 +57,19 @@ impl Terminal {
     pub fn cursor_show() {
         print!("{}", termion::cursor::Show);
     }
-
-    pub fn set_bg_color(color: color::Rgb){
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+    pub fn set_bg_color(color: color::Rgb) {
         print!("{}", color::Bg(color));
     }
-    pub fn reset_bg_color(){
+    pub fn reset_bg_color() {
         print!("{}", color::Bg(color::Reset));
     }
-
-    pub fn set_fg_color(color: color::Rgb){
+    pub fn set_fg_color(color: color::Rgb) {
         print!("{}", color::Fg(color));
     }
-    pub fn reset_fg_color(){
+    pub fn reset_fg_color() {
         print!("{}", color::Fg(color::Reset));
     }
-
 }
